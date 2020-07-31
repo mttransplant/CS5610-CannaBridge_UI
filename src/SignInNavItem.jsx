1ede5f0c69cf7c2302f75d8b07633f6c9339875b
apsmith <apsmith@ccs.neu.edu>
2020-07-30 21:58:51 -0400

    removed google sign in functionality

--- src/SignInNavItem.jsx
+++ src/SignInNavItem.jsx
@@ -18,72 +18,74 @@ class SigninNavItem extends React.Component {
   }
 
   componentDidMount() {
-    const clientId = window.ENV.GOOGLE_CLIENT_ID;
-    if (!clientId) return;
-    window.gapi.load('auth2', () => {
-      if (!window.gapi.auth2.getAuthInstance()) {
-        window.gapi.auth2.init({ client_id: clientId })
-          .then(() => {
-            this.setState({ disabled: false });
-          });
-      }
-    });
+    // const clientId = window.ENV.GOOGLE_CLIENT_ID;
+    // if (!clientId) return;
+    // window.gapi.load('auth2', () => {
+    //   if (!window.gapi.auth2.getAuthInstance()) {
+    //     window.gapi.auth2.init({ client_id: clientId })
+    //       .then(() => {
+    //         this.setState({ disabled: false });
+    //       });
+    //   }
+    // });
   }
 
   async signIn() {
     this.hideModal();
     const { showError } = this.props;
-    let googleToken;
-    try {
-      const auth2 = window.gapi.auth2.getAuthInstance();
-      const googleUser = await auth2.signIn();
-      googleToken = googleUser.getAuthResponse().id_token;
-    } catch (error) {
-      showError(`Error authenticating with Google: ${error.error}`);
-    }
+    // let googleToken;
+    // try {
+    //   const auth2 = window.gapi.auth2.getAuthInstance();
+    //   const googleUser = await auth2.signIn();
+    //   googleToken = googleUser.getAuthResponse().id_token;
+    // } catch (error) {
+    //   showError(`Error authenticating with Google: ${error.error}`);
+    // }
 
-    try {
-      const apiEndpoint = window.ENV.UI_AUTH_ENDPOINT;
-      const response = await fetch(`${apiEndpoint}/signin`, {
-        method: 'POST',
-        credentials: 'include',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ google_token: googleToken }),
-      });
-      const body = await response.text();
-      const result = JSON.parse(body);
-      const { signedIn, givenName } = result;
-      const { onUserChange } = this.props;
-      onUserChange({ signedIn, givenName });
-    } catch (error) {
-      showError(`Error signing into the app: ${error}`);
-    }
+    // try {
+    //   const apiEndpoint = window.ENV.UI_AUTH_ENDPOINT;
+    //   const response = await fetch(`${apiEndpoint}/signin`, {
+    //     method: 'POST',
+    //     credentials: 'include',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     body: JSON.stringify({ google_token: googleToken }),
+    //   });
+    //   const body = await response.text();
+    //   const result = JSON.parse(body);
+    //   const { signedIn, givenName } = result;
+    //   const { onUserChange } = this.props;
+    //   onUserChange({ signedIn, givenName });
+    // } catch (error) {
+    const error = 'this is not implemented yet';
+    showError(`Error signing into the app: ${error}`);
+    // }
   }
 
   async signOut() {
-    const apiEndpoint = window.ENV.UI_AUTH_ENDPOINT;
+    // const apiEndpoint = window.ENV.UI_AUTH_ENDPOINT;
     const { showError } = this.props;
-    try {
-      await fetch(`${apiEndpoint}/signout`, {
-        method: 'POST',
-        credentials: 'include',
-      });
-      const auth2 = window.gapi.auth2.getAuthInstance();
-      await auth2.signOut();
-      const { onUserChange } = this.props;
-      onUserChange({ signedIn: false, givenName: '' });
-    } catch (error) {
-      showError(`Error signing out: ${error}`);
-    }
+    // try {
+    //   await fetch(`${apiEndpoint}/signout`, {
+    //     method: 'POST',
+    //     credentials: 'include',
+    //   });
+    //   const auth2 = window.gapi.auth2.getAuthInstance();
+    //   await auth2.signOut();
+    //   const { onUserChange } = this.props;
+    //   onUserChange({ signedIn: false, givenName: '' });
+    // } catch (error) {
+    const error = 'this is not implemented yet';
+    showError(`Error signing out: ${error}`);
+    // }
   }
 
   showModal() {
-    const clientId = window.ENV.GOOGLE_CLIENT_ID;
-    const { showError } = this.props;
-    if (!clientId) {
-      showError('Missing environment variable GOOGLE_CLIENT_ID');
-      return;
-    }
+    // const clientId = window.ENV.GOOGLE_CLIENT_ID;
+    // const { showError } = this.props;
+    // if (!clientId) {
+    //   showError('Missing environment variable GOOGLE_CLIENT_ID');
+    //   return;
+    // }
     this.setState({ showing: true });
   }
 
@@ -115,7 +117,8 @@ class SigninNavItem extends React.Component {
               bsStyle="primary"
               onClick={this.signIn}
             >
-              <img src="https:/goo.gl/4yjp6B" alt="Sign In" />
+              Sign In – Not Implemented
+              {/* <img src="https:/goo.gl/4yjp6B" alt="Sign In" /> */}
             </Button>
           </Modal.Body>
           <Modal.Footer>
