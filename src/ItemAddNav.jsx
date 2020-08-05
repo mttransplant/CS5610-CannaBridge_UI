d91ca6377d3a22e734f400b1901402df84f278c0
apsmith <apsmith@ccs.neu.edu>
2020-08-05 19:51:06 -0400

    Linked ItemAddNav to new database

--- src/ItemAddNav.jsx
+++ src/ItemAddNav.jsx
@@ -5,6 +5,7 @@ import {
   ControlLabel, FormControl, Glyphicon, Tooltip, OverlayTrigger,
 } from 'react-bootstrap';
 import graphQLFetch from './graphQLFetch.js';
+import NumInput from './NumInput.jsx';
 import withToast from './withToast.jsx';
 
 class ItemAddNav extends React.Component {
@@ -12,10 +13,21 @@ class ItemAddNav extends React.Component {
     super(props);
     this.state = {
       showing: false,
+      item: {},
     };
+    this.onChange = this.onChange.bind(this);
     this.showModal = this.showModal.bind(this);
     this.hideModal = this.hideModal.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+  onChange(event, naturalValue) {
+    const { name, value: textValue } = event.target;
+    const value = naturalValue === undefined ? textValue : naturalValue;
+    this.setState(prevState => ({
+      item: { ...prevState.product, [name]: value },
+    }));
   }
 
   showModal() {
@@ -26,38 +38,50 @@ class ItemAddNav extends React.Component {
     this.setState({ showing: false });
   }
 
+
   async handleSubmit(e) {
     e.preventDefault();
     this.hideModal();
     const form = document.forms.itemAdd;
-    const issue = {
-      // TODO: update owner to quantity when new MongoDB is ready for #Iter2
-      owner: form.quantity.value,
-      title: form.title.value,
-      // TODO: Update due to something else useful when new MongoDB is ready
-      due: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 10),
-    };
-    // TODO: Update query for new MongoDB in #Iter2
-    // TODO: Use different queries (product vs request) depending on userType
-    const query = `mutation issueAdd($issue: IssueInputs!) {
-        issueAdd(issue: $issue) {
+    const { item } = this.state;
+    let query;
+    const { location: { pathname } } = this.props;
+    if (pathname === '/products') {
+      item.title = form.title.value;
+      // TODO: Update form to get type
+      item.type = 'Flower';
+      // TODO: update this to pull in current user
+      item.poster = 'Producer A';
+
+      query = `mutation productAdd($item: ProductInputs!) {
+        productAdd(product: $item) {
             id
         }
-    }`;
+      }`;
+    } else if (pathname === '/requests') {
+      item.title = form.title.value;
+      query = `mutation requestAdd($item: RequestInputs!) {
+        requestAdd(request: $item) {
+            id
+        }
+      }`;
+    }
 
     const { showError } = this.props;
     // TODO: Update issue to product in #Iter2
-    const data = await graphQLFetch(query, { issue }, showError);
+
+    const data = await graphQLFetch(query, { item }, showError);
     if (data) {
       const { history } = this.props;
       // TODO: Update issueAdd to productAdd in #Iter2
-      history.push(`/edit/product/${data.issueAdd.id}`);
+      history.push(`/edit/product/${data.productAdd.id}`);
     }
   }
 
   render() {
     const { showing } = this.state;
     // const { user: { signedIn } } = this.props;
+    const signedIn = true;
     // TODO: Uncomment next line when ready in #Iter2
     // const { user: { accountType } } = this.props;
     let toolTipId;
@@ -72,7 +96,7 @@ class ItemAddNav extends React.Component {
     }
     return (
       <React.Fragment>
-        <NavItem disabled={pathname !== '/products' && pathname !== '/requests'} onClick={this.showModal}>
+        <NavItem disabled={!signedIn && pathname !== '/products' && pathname !== '/requests'} onClick={this.showModal}>
           <OverlayTrigger
             placement="left"
             delayShow={1000}
@@ -93,7 +117,11 @@ class ItemAddNav extends React.Component {
               </FormGroup>
               <FormGroup>
                 <ControlLabel>Quantity</ControlLabel>
-                <FormControl name="quantity" />
+                <FormControl
+                  componentClass={NumInput}
+                  name="quantity"
+                  onChange={this.onChange}
+                />
               </FormGroup>
             </Form>
           </Modal.Body>
